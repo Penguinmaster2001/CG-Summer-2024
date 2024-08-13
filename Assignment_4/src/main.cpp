b13d3924ba3e0673caf06d20fa5dc5a90eb86d90
Anthony Cieri <penguinmillion@gmail.com>
2024-08-13 14:47:40 -0700

    feat: Done

--- Assignment_4/src/main.cpp
+++ Assignment_4/src/main.cpp
@@ -75,17 +75,29 @@ const std::string mesh_filename(data_dir + "dragon.off");
 
 
 //Camera settings
-const double focal_length = 2;
+const double focal_length = 5;
 const double field_of_view = 0.7854; //45 degrees
 const bool is_perspective = true;
 const Vector3d camera_position(0, 0, 2);
+
+
+
+// Maximum number of recursive calls
+// Reflection and refraction use this independently
+// So the true maximum is like 2^max_bounce or something idk
+const int max_bounce = 4;
 
 
 // Triangle Mesh
 MatrixXd vertices; // n x 3 matrix (n points)
 MatrixXi facets;   // m x 3 matrix (m triangles)
 AABBTree bvh;
-bool use_bvh_tree = true;
+const bool use_bvh_tree = true;
+
+// Objects
+std::vector<Vector3d> sphere_centers;
+std::vector<double> sphere_radii;
+std::vector<Matrix3d> parallelograms;
 
 
 //Material for the object, same material for all objects
@@ -94,6 +106,9 @@ const Vector4d obj_diffuse_color(0.5, 0.5, 0.5, 0);
 const Vector4d obj_specular_color(0.2, 0.2, 0.2, 0);
 const double obj_specular_exponent = 256.0;
 const Vector4d obj_reflection_color(0.7, 0.7, 0.7, 0);
+const Vector4d obj_refraction_color(0.7, 0.7, 0.7, 0);
+// Diamond
+const double obj_refractive_index = 2.42;
 
 
 // Precomputed (or otherwise) gradient vectors at each grid node
@@ -157,6 +172,129 @@ void setup_scene()
 
     light_positions.emplace_back(-4, 8, 0);
     light_colors.emplace_back(16, 16, 16, 0);
+
+    // I made this one bigger because there's this really random and weird bug that causes
+    // y1 to be 21 in perlin() and I don't want to figure out why
+    grid.resize(grid_size + 2);
+    for (int i = 0; i <= grid_size + 1; ++i)
+    {
+        grid[i].resize(grid_size + 2);
+        for (int j = 0; j <= grid_size + 1; ++j)
+            grid[i][j] = Vector2d::Random().normalized();
+    }
+
+    //Spheres
+    sphere_centers.emplace_back(10, 0, 1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(7, 0.05, -1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(4, 0.1, 1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(1, 0.2, -1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(-2, 0.4, 1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(-5, 0.8, -1);
+    sphere_radii.emplace_back(1);
+
+    sphere_centers.emplace_back(-8, 1.6, 1);
+    sphere_radii.emplace_back(1);
+
+    //parallelograms
+    parallelograms.emplace_back();
+    parallelograms.back() << -100, 100, -100,
+        -1.25, 0, -1.2,
+        -100, -100, 100;
+}
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Perlin noise code
+////////////////////////////////////////////////////////////////////////////////
+
+// Function to linearly interpolate between a0 and a1
+// Weight w should be in the range [0.0, 1.0]
+double lerp(double a0, double a1, double w)
+{
+    assert(w >= 0);
+    assert(w <= 1);
+    
+    // Linear
+    // return a0 + w * (a1 - a0);
+
+    // Cubic
+    return (a1 - a0) * (3.0 - w * 2.0) * w * w + a0;
+}
+
+
+
+// Computes the dot product of the distance and gradient vectors.
+double dotGridGradient(int ix, int iy, double x, double y)
+{
+    // Compute the distance vector
+    float dx = x - (float) ix;
+    float dy = y - (float) iy;
+
+    // Compute and return the dot-product
+    return (dx * grid[iy][ix][0]) + (dy * grid[iy][ix][1]);
+}
+
+
+
+// Compute Perlin noise at coordinates x, y
+double perlin(double x, double y)
+{
+    // Determine grid cell coordinates x0, y0
+    int x0 = int(x);
+    int x1 = x0 + 1;
+    int y0 = int(y);
+    int y1 = y0 + 1;
+
+    // Determine interpolation weights
+    double sx = x - x0;
+    double sy = y - y0;
+
+    // Interpolate between grid point gradients
+    double n0 = dotGridGradient(x0, y0, x, y);
+    double n1 = dotGridGradient(x1, y0, x, y);
+
+    double ix0 = lerp(n0, n1, sx);
+
+    n0 = dotGridGradient(x0, y1, x, y);
+    n1 = dotGridGradient(x1, y1, x, y);
+
+    double ix1 = lerp(n0, n1, sx);
+    double value = lerp(ix0, ix1, sy);
+
+    return value;
+}
+
+
+
+Vector4d procedural_texture(const double tu, const double tv)
+{
+    assert(tu >= 0);
+    assert(tv >= 0);
+
+    assert(tu <= 1);
+    assert(tv <= 1);
+
+    // Uncomment these lines once you implement the perlin noise
+    const double color = (perlin(tu * grid_size, tv * grid_size) + 1) / 2;
+
+    return Vector4d(0, color, 0, 0);
+
+    // Example of checkerboard texture
+    // const double color = (int(tu * grid_size) + int(tv * grid_size)) % 2 == 0 ? 0 : 1;
+    // return Vector4d(0, color, 0, 0);
 }
 
 
@@ -303,6 +441,35 @@ int AABBTree::build_recursive(const MatrixXd &V, const MatrixXi &F, const Matrix
 ////////////////////////////////////////////////////////////////////////////////
 
 
+//Compute the intersection between a ray and a sphere, return -1 if no intersection
+double ray_sphere_intersection(const Vector3d &ray_origin, const Vector3d &ray_direction, int index, Vector3d &p, Vector3d &N)
+{
+    //return t or -1 if no intersection
+
+    // From Assignment 2
+
+    const Vector3d sphere_center = sphere_centers[index];
+    const double sphere_radius = sphere_radii[index];
+
+    // Moving the origin
+    Vector3d c = ray_origin - sphere_center;
+
+    double ray_dir_dot_c = ray_direction.dot(c);
+    double ray_dir_sqr = ray_direction.dot(ray_direction);
+
+    double discriminant = (ray_dir_dot_c * ray_dir_dot_c) - (ray_dir_sqr * (c.dot(c) - (sphere_radius * sphere_radius)));
+    if (discriminant < 0.0) return -1;
+
+    double t = (-ray_dir_dot_c - std::sqrt(discriminant)) / ray_dir_sqr;
+    if (t < 0.0) return -1;
+
+    p = ray_origin + (t * ray_direction);
+    N = (p - sphere_center) / sphere_radius;
+
+    return t;
+}
+
+
 double det_of_3_col_vectors(Vector3d a, Vector3d b, Vector3d c)
 {
     return a.x() * (b.y() * c.z() - c.y() * b.z())
@@ -350,6 +517,41 @@ double ray_triangle_intersection(const Vector3d &ray_origin, const Vector3d &ray
 
     p = ray_origin + (t * ray_direction);
     N = v.cross(u).normalized();
+    return t;
+}
+
+
+
+//Compute the intersection between a ray and a parallelogram, return -1 if no intersection
+// From Assignment 2
+double ray_parallelogram_intersection(const Vector3d &ray_origin, const Vector3d &ray_direction, int index, Vector3d &p, Vector3d &N)
+{
+    //return t or -1 if no intersection
+
+    const Vector3d pgram_origin = parallelograms[index].col(0);
+    const Vector3d A = parallelograms[index].col(1);
+    const Vector3d B = parallelograms[index].col(2);
+    const Vector3d pgram_u = A - pgram_origin;
+    const Vector3d pgram_v = B - pgram_origin;
+
+
+    // To fix the confusion around the definition of the parallelogram
+    const Vector3d o = pgram_origin;
+    const Vector3d u = pgram_origin + pgram_u;
+    const Vector3d v = pgram_origin + pgram_v;
+
+
+    // A parallelogram is just two triangles
+    // First tri:
+    // o -> u -> v
+    double t = ray_triangle_intersection(ray_origin, ray_direction, o, u, v, p, N);
+
+    if (t >= 0) return t;
+
+    // Second tri:
+    // v -> o -> v + u - o
+    t = ray_triangle_intersection(ray_origin, ray_direction, v, u, v + u - o, p, N);
+
     return t;
 }
 
@@ -418,76 +620,117 @@ bool find_nearest_object(const Vector3d &ray_origin, const Vector3d &ray_directi
                 }
             }
         }
-
-        return intersect;
     }
-
-    // TODO
-    // Method (2): Traverse the BVH tree and test the intersection with a
-    // triangles at the leaf nodes that intersects the input ray.
-
-    // Level order traversal
-
-    // The tree is empty
-    if (bvh.root < 0) return false;
-
-    std::queue<AABBTree::Node> current_nodes;
-
-    current_nodes.push(bvh.nodes[bvh.root]);
-    
-    while (!current_nodes.empty())
+    else
     {
-        AABBTree::Node current_node = current_nodes.front();
+        // Method (2): Traverse the BVH tree and test the intersection with a
+        // triangles at the leaf nodes that intersects the input ray.
 
-        // Found the triangle thingy
-        if (current_node.triangle >= 0)
+        // Level order traversal
+
+        // The tree is empty
+        if (bvh.root < 0) return false;
+
+        std::queue<AABBTree::Node> current_nodes;
+
+        current_nodes.push(bvh.nodes[bvh.root]);
+        
+        while (!current_nodes.empty())
         {
-            int vert_ind0 = facets(current_node.triangle, 0);
-            int vert_ind1 = facets(current_node.triangle, 1);
-            int vert_ind2 = facets(current_node.triangle, 2);
+            AABBTree::Node current_node = current_nodes.front();
 
-            //returns t and writes on tmp_p and tmp_N
-            const double t = ray_triangle_intersection(ray_origin, ray_direction,
-                vertices.row(vert_ind0), vertices.row(vert_ind1), vertices.row(vert_ind2), tmp_p, tmp_N);
-            
-            //We have intersection
-            if (t >= 0)
+            // Found the triangle thingy
+            if (current_node.triangle >= 0)
             {
-                //The point is before our current closest t
-                if (t < closest_t)
+                int vert_ind0 = facets(current_node.triangle, 0);
+                int vert_ind1 = facets(current_node.triangle, 1);
+                int vert_ind2 = facets(current_node.triangle, 2);
+
+                //returns t and writes on tmp_p and tmp_N
+                const double t = ray_triangle_intersection(ray_origin, ray_direction,
+                    vertices.row(vert_ind0), vertices.row(vert_ind1), vertices.row(vert_ind2), tmp_p, tmp_N);
+                
+                //We have intersection
+                if (t >= 0)
                 {
-                    intersect = true;
-                    closest_t = t;
-                    p = tmp_p;
-                    N = tmp_N;
+                    //The point is before our current closest t
+                    if (t < closest_t)
+                    {
+                        intersect = true;
+                        closest_t = t;
+                        p = tmp_p;
+                        N = tmp_N;
+                    }
+                }
+            }
+
+            current_nodes.pop();
+
+            // Enqueue left child
+            if (current_node.left >= 0)
+            {
+                AABBTree::Node left_node = bvh.nodes[current_node.left];
+
+                if (ray_box_intersection(ray_origin, ray_direction, left_node.bbox))
+                {
+                    current_nodes.push(left_node);
+                }
+            }
+
+            // Enqueue right child
+            if (current_node.right >= 0)
+            {
+                AABBTree::Node right_node = bvh.nodes[current_node.right];
+
+                if (ray_box_intersection(ray_origin, ray_direction, right_node.bbox))
+                {
+                    current_nodes.push(right_node);
                 }
             }
         }
+    }
 
-        current_nodes.pop();
 
-        // Enqueue left child
-        if (current_node.left >= 0)
+
+    for (int i = 0; i < sphere_centers.size(); ++i)
+    {
+        //returns t and writes on tmp_p and tmp_N
+        const double t = ray_sphere_intersection(ray_origin, ray_direction, i, tmp_p, tmp_N);
+        //We have intersection
+        if (t >= 0)
         {
-            AABBTree::Node left_node = bvh.nodes[current_node.left];
-
-            if (ray_box_intersection(ray_origin, ray_direction, left_node.bbox))
+            //The point is before our current closest t
+            if (t < closest_t)
             {
-                current_nodes.push(left_node);
-            }
-        }
+                intersect = true;
 
-        // Enqueue right child
-        if (current_node.right >= 0)
-        {
-            AABBTree::Node right_node = bvh.nodes[current_node.right];
-
-            if (ray_box_intersection(ray_origin, ray_direction, right_node.bbox))
-            {
-                current_nodes.push(right_node);
+                closest_t = t;
+                p = tmp_p;
+                N = tmp_N;
             }
         }
     }
+
+
+    for (int i = 0; i < parallelograms.size(); ++i)
+    {
+        //returns t and writes on tmp_p and tmp_N
+        const double t = ray_parallelogram_intersection(ray_origin, ray_direction, i, tmp_p, tmp_N);
+        //We have intersection
+        if (t >= 0)
+        {
+            //The point is before our current closest t
+            if (t < closest_t)
+            {
+                intersect = true;
+
+                closest_t = t;
+                p = tmp_p;
+                N = tmp_N;
+            }
+        }
+    }
+
 
     return intersect;
 }
@@ -500,7 +743,38 @@ bool find_nearest_object(const Vector3d &ray_origin, const Vector3d &ray_directi
 ////////////////////////////////////////////////////////////////////////////////
 
 
-Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction)
+//Checks if the light is visible
+bool is_light_visible(const Vector3d &ray_origin, const Vector3d &ray_direction, const Vector3d &light_position)
+{
+    // Determine if the light is visible here
+    // Use find_nearest_object
+    Vector3d p, N;
+
+    // -1 means no object was hit so the light is visible.
+    return (find_nearest_object(ray_origin, ray_direction, p, N) < 0);
+}
+
+
+
+// Returns true and sets the direction if refracting
+// false otherwise
+bool calc_refraction(const Vector3d &ray_direction, const Vector3d &N, Vector3d &refraction_direction)
+{
+    double d_dot_n = ray_direction.dot(N);
+
+    double det = 1.0 - ((obj_refractive_index * obj_refractive_index) * (1.0 - (d_dot_n * d_dot_n)));
+
+    // Total internal reflection
+    if (det < 0.0) return false;
+
+    refraction_direction = (obj_refractive_index * (ray_direction - (d_dot_n * N)))
+        - det * N;
+
+    return true;
+}
+
+
+Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction, int bounce)
 {
     //Intersection point and normal, these are output of find_nearest_object
     Vector3d p, N;
@@ -513,6 +787,10 @@ Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction)
         return Vector4d(0, 0, 0, 0);
     }
 
+    N = N.normalized();
+    Vector3d offset_p = p + (EPSILON * N);
+    Vector3d internal_offset_p = p - (EPSILON * N);
+
     // Ambient light contribution
     const Vector4d ambient_color = obj_ambient_color.array() * ambient_light.array();
 
@@ -523,10 +801,30 @@ Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction)
         const Vector3d &light_position = light_positions[i];
         const Vector4d &light_color = light_colors[i];
 
+        // Shadow ray direction
+        const Vector3d Li = (light_position - p).normalized();
+
+        // Shoot a shadow ray to determine if the light should affect the intersection point and call is_light_visible
+        if (!is_light_visible(offset_p, Li, light_position)) continue;
+
+
         Vector4d diff_color = obj_diffuse_color;
 
+        //Compute UV coordinates for the point on the sphere
+        // const double x = p(0) - sphere_centers[nearest_object][0];
+        // const double y = p(1) - sphere_centers[nearest_object][1];
+        // const double z = p(2) - sphere_centers[nearest_object][2];
+        // double tu = acos(z / sphere_radii[nearest_object]) / 3.1415;
+        // double tv = (3.1415 + atan2(y, x)) / (2 * 3.1415);
+        // tu = std::min(tu, 1.0);
+        // tu = std::max(tu, 0.0);
+
+        // tv = std::min(tv, 1.0);
+        // tv = std::max(tv, 0.0);
+
+        // diff_color = procedural_texture(tu, tv);
+
         // Diffuse contribution
-        const Vector3d Li = (light_position - p).normalized();
         const Vector4d diffuse = diff_color * std::max(Li.dot(N), 0.0);
 
         // Specular contribution
@@ -539,8 +837,27 @@ Vector4d shoot_ray(const Vector3d &ray_origin, const Vector3d &ray_direction)
         lights_color += (diffuse + specular).cwiseProduct(light_color) / D.squaredNorm();
     }
 
+    Vector4d refl_color = obj_reflection_color;
+
+
+    Vector3d reflection_direction = ray_direction - ((2.0 * ray_direction.dot(N)) * N);
+
+    Vector3d refraction_direction;
+
+    // Compute the color of the reflected ray and add its contribution to the current point color.
+    // use refl_color
+    Vector4d reflection_color = bounce > 0
+        ? refl_color.cwiseProduct(shoot_ray(offset_p, reflection_direction, bounce - 1))
+        : Vector4d(0, 0, 0, 0);
+
+    // Compute the color of the refracted ray and add its contribution to the current point color.
+    // Make sure to check for total internal reflection before shooting a new ray.
+    Vector4d refraction_color = (bounce > 0 && calc_refraction(ray_direction, N, refraction_direction))
+        ? obj_refraction_color.cwiseProduct(shoot_ray(internal_offset_p, refraction_direction, bounce - 1))
+        : Vector4d(0, 0, 0, 0);
+
     // Rendering equation
-    Vector4d C = ambient_color + lights_color;
+    Vector4d C = ambient_color + lights_color + reflection_color + refraction_color;
 
     //Set alpha to 1
     C(3) = 1;
@@ -558,8 +875,8 @@ void raytrace_scene()
 {
     std::cout << "Simple ray tracer." << std::endl;
 
-    int w = 640;
-    int h = 480;
+    const int w = 640;
+    const int h = 480;
     MatrixXd R = MatrixXd::Zero(w, h);
     MatrixXd G = MatrixXd::Zero(w, h);
     MatrixXd B = MatrixXd::Zero(w, h);
@@ -568,9 +885,9 @@ void raytrace_scene()
     // The camera always points in the direction -z
     // The sensor grid is at a distance 'focal_length' from the camera center,
     // and covers an viewing angle given by 'field_of_view'.
-    double aspect_ratio = double(w) / double(h);
-    double image_y = tan(field_of_view / 2.0) * focal_length; // Compute the correct pixels size
-    double image_x = image_y * aspect_ratio; // Compute the correct pixels size
+    const double aspect_ratio = double(w) / double(h);
+    const double image_y = tan(field_of_view / 2.0) * focal_length; // Compute the correct pixels size
+    const double image_x = image_y * aspect_ratio; // Compute the correct pixels size
 
     // The pixel grid through which we shoot rays is at a distance 'focal_length'
     const Vector3d image_origin(-image_x, image_y, camera_position[2] - focal_length);
@@ -600,16 +917,21 @@ void raytrace_scene()
                 ray_direction = Vector3d(0, 0, -1);
             }
 
-            const Vector4d C = shoot_ray(ray_origin, ray_direction);
+            const Vector4d C = shoot_ray(ray_origin, ray_direction, max_bounce);
             R(i, j) = C(0);
             G(i, j) = C(1);
             B(i, j) = C(2);
             A(i, j) = C(3);
         }
+
+        std::cout << (100.0 * double(i) / double(w)) << "\% done (" << i << " / " << w << " rows)\n";
     }
 
     // Save to png
     write_matrix_to_png(R, G, B, A, filename);
+
+    // For completeness
+    std::cout << "100\% done!" << std::endl;
 }
 
 
